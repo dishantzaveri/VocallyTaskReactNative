ffa3f9f2f4a8704f6118045d5c6a8cf2664cbeb7
dishantzaveri <zaveridishant@gmail.com>
2022-07-09 00:08:11 +0530

    setup

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Splash from './src/screens/Splash';
+import BottomTabs from './src/bottomtabs';
+import Location from './src/location';
+import AddProject from './src/AddProject';
+
+const Stack = createStackNavigator();
+function App() {
+  useEffect(() => {
+    console.disableYellowBox = true
+  })
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" independent={true}>
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen
+          name="bottomtabs"
+          component={BottomTabs}
+          options={{ headerShown: false }}></Stack.Screen>
+      <Stack.Screen
+          name="AddProject"
+          component={AddProject}
+          options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen
+          name="Location"
+          component={Location}
+          options={{ headerShown: false }}></Stack.Screen>
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/screens/location.js
@@ -0,0 +1,111 @@
+import React,{useState , useRef,useEffect }  from "react";
+import { StyleSheet,Button, Text, View,TouchableOpacity } from "react-native";
+import { Marker } from "react-native-maps";
+import MapView from "react-native-maps";
+import {URL,token} from "../utils/link";
+export default function Location({navigation}) {
+  const [data,setData]=useState([]);
+  const [loading,setLoading]=useState(true);
+  const [visible,setVisible]=useState(false);
+  const mapRef = useRef(null);
+  const [region, setRegion] = useState({
+    latitude: 19.0760,
+    longitude: 72.8777,
+    latitudeDelta: 0.001,
+    longitudeDelta: 0.001,
+  });
+  const mumbaiRegion = {
+    latitude: 19.0760,
+    longitude: 72.8777,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+
+  const getProjects=async()=>{
+    setLoading(true);
+    try{
+        const result=await fetch(URL+'/create_project/',{
+            method:'GET',
+            headers: {'Authorization': 'Token '+token},
+        });
+        const json= await result.json();
+        console.log(json);
+        setData(json);
+    }catch(error){
+        console.log(error);
+        // Alert.alert(error);
+    }finally{
+        setLoading(false);
+    }
+}
+  const goToTokyo = () => {
+    //complete this animation in 3 seconds
+    mapRef.current.animateToRegion(tokyoRegion, 3 * 1000);
+  };
+  const selectLocation = () => {
+    setVisible(true);
+    // navigation.navigate('AddProject')
+  };
+
+  useEffect(() => {
+    // console.log(BASE_URL);
+      getProjects();
+  },[]);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        initialRegion={{
+          latitude: 19.0760,
+          longitude: 72.8777,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        onRegionChangeComplete={(region) => setRegion(region)}
+      >
+      <Marker coordinate={mumbaiRegion} title="Mumbai" onPress={()=>navigation.navigate('Details')}/>
+      {data.map((item,index)=>(
+        <Marker key={index} title={item.name} coordinate={{
+          latitude: item.latitude,
+          longitude: item.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        onPress={()=>navigation.navigate('Details',{id:item.id,loc:item.location_name,desc:item.description,name:item.name})}
+        />
+      ))}
+      {visible?<Marker coordinate={region} onPress={()=>navigation.navigate("AddProject",{loc:region})} pinColor="green" title="New Project"/>:<></>}
+      </MapView>
+      <View>
+        <TouchableOpacity style={styles.button} onPress={() => selectLocation()}><Text style={{color:'white',fontSize:18}}>Add A Project</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => getProjects()}><Text style={{color:'white',fontSize:16}}>Refresh</Text></TouchableOpacity>
+      </View>
+      {/* <Text style={styles.text}>Current latitude{region.latitude}</Text>
+      <Text style={styles.text}>Current longitude{region.longitude}</Text> */}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  text: {
+    fontSize: 20,
+    backgroundColor: "lightblue",
+  },
+  button:{
+    width:150,
+    alignItems:'center',
+    margin:10,
+    backgroundColor:'#0065ff',
+    borderRadius:7,
+  }
+});
