4cf6f4618e4e67c0ae14a9239f53ab4cbc3df735
Dishant Zaveri <80118978+dishantzaveri@users.noreply.github.com>
2022-08-20 14:18:55 +0530

    Delete link.js

--- src/utils/link.js
+++
@@ -1,3 +0,0 @@
-export const URL="http://6612-171-77-154-123.ngrok.io";
-// export const token="b01b48aeb6761f6b2b4d095f3b81eace9cf42f9a";
-export const token="e42d4ce6f40382b481504ba7d251b10b05b069b6";
